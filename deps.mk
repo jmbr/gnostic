376d33519977d072829843d92dc307cb21eac46a
Juan M. Bello Rivas <rwx@synnergy.net>
2004-07-18 00:23:30 +0200

    Removed cleanup stack exception handling module.
    Keywords:
    
    (rwx@synnergy.net--2004-warez/gnostic--mainline--0.1--patch-16)
    
    darcs-hash:20040717222330-9328d-dde8c71906bdffc0ec74d14f94ea2d1ce355852b.gz

--- deps.mk
+++ deps.mk
@@ -4,7 +4,11 @@ debug.o: debug.c config.h
 exec.o: exec.c config.h exec.h task.h ast.h xalloc.h debug.h
 gnostic.o: gnostic.c config.h gnostic.h task.h ast.h htab.h graph.h \
   exec.h version.h debug.h xalloc.h
+grammar.o: grammar.c config.h task.h ast.h scanner.h xalloc.h
 graph.o: graph.c config.h graph.h graph-priv.h debug.h xalloc.h
 htab.o: htab.c config.h htab.h
+parser.o: parser.c config.h gnostic.h task.h ast.h htab.h graph.h \
+  scanner.h grammar.h xalloc.h debug.h
+scanner.o: scanner.c config.h grammar.h xalloc.h debug.h
 task.o: task.c config.h task.h ast.h xalloc.h
 xalloc.o: xalloc.c config.h debug.h
