3c4da0b381f7a158f641882763eb5854dfef1a2b
Juan M. Bello Rivas <rwx@synnergy.net>
2004-07-18 11:39:32 +0200

    Removed all the dependencies on cleanup.c from the make file.
    Keywords:
    
    (rwx@synnergy.net--2004-warez/gnostic--mainline--0.1--patch-18)
    
    darcs-hash:20040718093932-9328d-c49bb1ead70d8f7dada84507a15fa69be8fb598c.gz

--- deps.mk
+++ deps.mk
@@ -1,10 +1,9 @@
 ast.o: ast.c config.h ast.h xalloc.h
-cleanup.o: cleanup.c config.h xalloc.h cleanup.h
 debug.o: debug.c config.h
 exec.o: exec.c config.h exec.h task.h ast.h xalloc.h debug.h
 gnostic.o: gnostic.c config.h gnostic.h task.h ast.h htab.h graph.h \
   exec.h version.h debug.h xalloc.h
-grammar.o: grammar.c config.h task.h ast.h scanner.h xalloc.h
+grammar.o: grammar.c config.h ast.h scanner.h xalloc.h
 graph.o: graph.c config.h graph.h graph-priv.h debug.h xalloc.h
 htab.o: htab.c config.h htab.h
 parser.o: parser.c config.h gnostic.h task.h ast.h htab.h graph.h \
