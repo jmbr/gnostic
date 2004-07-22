3827eb3dae2c8b11e5bd7ef8d6fc26eb0f822f9d
Juan M. Bello Rivas <rwx@synnergy.net>
2004-07-22 15:57:28 +0200

    Flagged fatal_error as a non-returning function for GCC.
    Keywords:
    
    (rwx@synnergy.net--2004-warez/gnostic--mainline--0.1--patch-25)
    
    darcs-hash:20040722135728-9328d-53e7875dc71a12afb37e36f080847b58606d6a69.gz

--- debug.h
+++ debug.h
@@ -1,5 +1,5 @@
-#ifndef GNOSTIC_DEBUG_H
-#define GNOSTIC_DEBUG_H			1
+#ifndef DEBUG_H
+#define DEBUG_H			1
 /*
  * debug.h -- Misc. debugging features.
  * $Id: debug.h,v 1.1.1.1 2001/12/07 20:56:20 rwx Exp $
@@ -8,7 +8,7 @@
 
 extern void dprintf(const char *format, ...);
 extern void eprintf(const char *format, ...);
-extern void fatal_error(const char *format, ...);
+extern void fatal_error(const char *format, ...) __attribute__ ((noreturn));
 
 
-#endif /* GNOSTIC_DEBUG_H */
+#endif /* DEBUG_H */
