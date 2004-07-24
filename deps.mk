60c1317633fec577bee8ea9042b5b54fe20379eb
Juan M. Bello Rivas <rwx@synnergy.net>
2004-07-24 14:04:00 +0200

    Updated package configuration script.
    Keywords:
    
    (rwx@synnergy.net--2004-warez/gnostic--mainline--0.1--patch-48)
    
    darcs-hash:20040724120400-9328d-dd24d198d5ad05a3799c18ac5696e5407060a2cc.gz

--- deps.mk
+++ deps.mk
@@ -1,13 +1,13 @@
 ast.o: ast.c config.h ast.h xalloc.h
 debug.o: debug.c config.h
-exec.o: exec.c config.h exec.h task.h ast.h xalloc.h debug.h
+exec.o: exec.c config.h exec.h task.h ast.h debug.h
 gnostic.o: gnostic.c config.h gnostic.h task.h ast.h htab.h graph.h \
   exec.h version.h debug.h xalloc.h
-grammar.o: grammar.c config.h task.h ast.h scanner.h xalloc.h
+grammar.o: grammar.c config.h task.h ast.h scanner.h xalloc.h debug.h
 graph.o: graph.c config.h graph.h graph-priv.h debug.h xalloc.h
 htab.o: htab.c config.h htab.h
 parser.o: parser.c config.h gnostic.h task.h ast.h htab.h graph.h \
   scanner.h grammar.h xalloc.h debug.h
 scanner.o: scanner.c config.h grammar.h xalloc.h debug.h
 task.o: task.c config.h task.h ast.h xalloc.h
-xalloc.o: xalloc.c config.h debug.h
+xalloc.o: xalloc.c config.h
