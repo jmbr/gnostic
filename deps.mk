6dbed7161e7dc5df625f85ca4bb8576b6b681fed
Juan M. Bello Rivas <rwx@synnergy.net>
2004-08-17 00:44:44 +0200

    Updated house-keeping files.
    Keywords:
    
    (rwx@synnergy.net--2004-warez/gnostic--mainline--0.1--patch-146)
    
    darcs-hash:20040816224444-9328d-fc5484d73aa3373a0769d10242d86e4891b624f0.gz

--- deps.mk
+++ deps.mk
@@ -1,11 +1,13 @@
-ast.o: ast.c config.h ast.h xmemory.h
+ast.o: ast.c config.h ast-priv.h ast.h xmemory.h
+ast-itor.o: ast-itor.c config.h ast-priv.h ast.h stack.h xmemory.h
 err.o: err.c config.h
 gnostic.o: gnostic.c config.h taskset.h task.h ast.h vars.h version.h \
   err.h
 grammar.o: grammar.c config.h tasklist.h task.h ast.h taskset.h vars.h \
   err.h xmemory.h
-hashtab.o: hashtab.c config.h hashtab.h
+hashtab.o: hashtab.c config.h hashtab.h xmemory.h
 scanner.o: scanner.c config.h grammar.h err.h xmemory.h
+stack.o: stack.c config.h stack.h xmemory.h
 task.o: task.c config.h task.h ast.h err.h xmemory.h
 task-exec.o: task-exec.c config.h task.h ast.h err.h
 tasklist.o: tasklist.c config.h tasklist.h task.h ast.h xmemory.h
